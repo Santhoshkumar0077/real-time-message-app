4dc6a22b4c205a0c4136c0f0d315e5b2c8424ba5
Santhoshkumar0077 <santhoshkumarmanoharan@gmail.com>
2025-01-31 06:46:03 +0530

    front end socket url changes

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://real-time-message-app-kbej.onrender.com"
+  baseURL: "/"
 });
 
 export default axiosInstance;
